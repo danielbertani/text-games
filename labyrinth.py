2323cc1e6e6a28a9ada01120e7c98c501a18b147
danielbertani <daniel.bertani.okland@gmail.com>
2024-06-06 22:18:43 +0200

    reformated with black

--- labyrinth.py
+++ labyrinth.py
@@ -1,10 +1,7 @@
-# Inspired by: 
-# https://reintech.io/blog/how-to-create-a-text-based-adventure-game-with-python
-# Modified to create my own game
-
+# source: https://reintech.io/blog/how-to-create-a-text-based-adventure-game-with-python
 rooms = {
-    'start': {
-        'description': """You are trapped in a dark labyrinth. To move you can use the following keys:
+    "start": {
+        "description": """You are trapped in a dark labyrinth. To move you can use the following keys:
           f - forward
           b - back
           r - right
@@ -12,91 +9,94 @@ rooms = {
           e - enter
         What would you like to do?
         """,
-        'forward': 'room1',
-        'right': 'room5'
+        "forward": "room1",
+        "right": "room5",
     },
-    'room1': {
-        'description': '\nYou hear the scraping sound of scithe against stone.',
-        'forward': 'room2',
-        'back': 'start'
+    "room1": {
+        "description": "\nYou hear the scraping sound of scithe against stone.",
+        "forward": "room2",
+        "back": "start",
     },
-    'room2': {
-        'description': '\nYou continue exploring the labyrinth.',
-        'right': 'room3',
-        'back': 'room1'
+    "room2": {
+        "description": "\nYou continue exploring the labyrinth.",
+        "right": "room3",
+        "back": "room1",
     },
-    'room3': {
-        'description': '\nYou found a candle lighting dimly the way.',
-        'forward': 'room4',
-        'back': 'room2'
+    "room3": {
+        "description": "\nYou found a candle lighting dimly the way.",
+        "forward": "room4",
+        "back": "room2",
     },
-    'room5': {
-        'description': '\nThere is a picture in the wall of an old man with his two daughters. The man in the picture is staring at you.',
-        'forward': 'room6',
-        'back': 'start'
-    },    
-    'room6': {
-        'description': '\nYou see a mysterious figure standing in the dark with red glowing eyes staring at you. It is not approaching you.',
-        'left': 'room7',
-        'back': 'room5'
+    "room5": {
+        "description": "\nThere is a picture in the wall of an old man with his two daughters. The man in the picture is staring at you.",
+        "forward": "room6",
+        "back": "start",
     },
-    'room7': {
-        'description': '\nYou se a dim blue glowing light to your left emiting a strange aura.',
-        'left': 'enter',
-        'back': 'room6'
+    "room6": {
+        "description": "\nYou see a mysterious figure standing in the dark with red glowing eyes staring at you. It is not approaching you.",
+        "left": "room7",
+        "back": "room5",
     },
-    'enter': {
-        'description': '\nYou found the source of the light. It is a blue glowing portal. Do you want to enter? It can be an exit or a trap, but it is the only way you got. ',
-        'enter': 'start_2',
-        'back': 'room7'
+    "room7": {
+        "description": "\nYou se a dim blue glowing light to your left emiting a strange aura.",
+        "left": "enter",
+        "back": "room6",
     },
-    'start_2': {
-        'description': '\nOh no! You are stuck in a new bigger labyrith. Good luck! Try to find the next portal. Maybe it is the exit this time.',
-        'end': True
+    "enter": {
+        "description": "\nYou found the source of the light. It is a blue glowing portal. Do you want to enter? It can be an exit or a trap, but it is the only way you got. ",
+        "enter": "start_2",
+        "back": "room7",
     },
-    'room4': {
-        'description': '\nYou found a dead end, guarded by a Lich. It raised its scythe and you got killed by it. Better luck next time!',
-        'back': 'room3',
-        'end': True
-    }
+    "start_2": {
+        "description": "\nOh no! You are stuck in a new bigger labyrith. Good luck! Try to find the next portal. Maybe it is the exit this time.",
+        "end": True,
+    },
+    "room4": {
+        "description": "\nYou found a dead end, guarded by a Lich. It raised its scythe and you got killed by it. Better luck next time!",
+        "back": "room3",
+        "end": True,
+    },
 }
 
-def show_room(room):
-    print(room['description'])
 
-    if 'forward' in room:
-        print('There is a path to move forward.')
-    if 'back' in room:
-        print('There is a path to go back.')
-    if 'left' in room:
-        print('There is a path to your left.')
-    if 'right' in room:
-        print('There is a path to your right.')
-    if 'enter' in room:
-        print('You can enter the portal.')
+def show_room(room):
+    print(room["description"])
+
+    if "forward" in room:
+        print("There is a path to move forward.")
+    if "back" in room:
+        print("There is a path to go back.")
+    if "left" in room:
+        print("There is a path to your left.")
+    if "right" in room:
+        print("There is a path to your right.")
+    if "enter" in room:
+        print("You can enter the portal.")
+
 
 def get_action(room):
     while True:
-        action = input('What do you want to do? ').lower().strip()
+        action = input("What do you want to do? ").lower().strip()
 
-        if action == 'f' and 'forward' in room:
-            return room['forward']
-        elif action == 'l' and 'left' in room:
-            return room['left']
-        elif action == 'b' and 'back' in room:
-            return room['back']
-        elif action == 'r' and 'right' in room:
-            return room['right']
-        elif action == 'e' and 'enter' in room:
-            return room['enter']
+        if action == "f" and "forward" in room:
+            return room["forward"]
+        elif action == "l" and "left" in room:
+            return room["left"]
+        elif action == "b" and "back" in room:
+            return room["back"]
+        elif action == "r" and "right" in room:
+            return room["right"]
+        elif action == "e" and "enter" in room:
+            return room["enter"]
         else:
-            print('Invalid action. Try again.')
+            print("Invalid action. Try again.")
 
-current_room = rooms['start']
 
-while 'end' not in current_room:
+current_room = rooms["start"]
+
+while "end" not in current_room:
     show_room(current_room)
     next_room = get_action(current_room)
     current_room = rooms[next_room]
 
-print(current_room['description'])
+print(current_room["description"])
