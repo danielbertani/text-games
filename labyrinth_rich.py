ef560448a17e84a7f5e7206de6169de37fdd9c84
danielbertani <daniel.bertani.okland@gmail.com>
2024-06-06 22:45:12 +0200

    added colors to the terminal

--- labyrinth_rich.py
+++ labyrinth_rich.py
@@ -1,21 +1,25 @@
 # source: https://reintech.io/blog/how-to-create-a-text-based-adventure-game-with-python
 # Modified to make my own text game
 
+from rich.console import Console
+
+console = Console()
+
 rooms = {
     "start": {
-        "description": """You are trapped in a dark labyrinth. To move you can use the following keys:
-          f - forward
-          b - back
-          r - right
-          l - left
-          e - enter
+        "description": """You are trapped in a [bold cyan]dark labyrinth[/bold cyan]. To move you can use the following keys:
+          [bold]f[/bold] - forward
+          [bold]b[/bold] - back
+          [bold]r[/bold] - right
+          [bold]l[/bold] - left
+          [bold]e[/bold] - enter
         What would you like to do?
         """,
         "forward": "room1",
         "right": "room5",
     },
     "room1": {
-        "description": "\nYou hear the scraping sound of scithe against stone.",
+        "description": "\nYou hear the scraping sound of [bold red]scithe[/bold red] against stone.",
         "forward": "room2",
         "back": "start",
     },
@@ -40,21 +44,21 @@ rooms = {
         "back": "room5",
     },
     "room7": {
-        "description": "\nYou se a dim blue glowing light to your left emiting a strange aura.",
+        "description": "\nYou se a dim [bold blue]blue glowing[/bold blue] light to your left emiting a strange aura.",
         "left": "enter",
         "back": "room6",
     },
     "enter": {
-        "description": "\nYou found the source of the light. It is a blue glowing portal. Do you want to enter? It can be an exit or a trap, but it is the only way you got. ",
+        "description": "\nYou found the source of the light. It is a [bold blue]blue glowing[/bold blue] portal. Do you want to enter? It can be an exit or a trap, but it is the only way you got. ",
         "enter": "start_2",
         "back": "room7",
     },
     "start_2": {
-        "description": "\nOh no! You are stuck in a new bigger labyrith. Good luck! Try to find the next portal. Maybe it is the exit this time.",
+        "description": "\nOh no! You are stuck in a new bigger [bold cyan]labyrith[/bold cyan]. Good luck! Try to find the next portal. Maybe it is the exit this time.",
         "end": True,
     },
     "room4": {
-        "description": "\nYou found a dead end, guarded by a Lich. It raised its scythe and you got killed by it. Better luck next time!",
+        "description": "\nYou found a dead end, guarded by a [bold red]Lich[/bold red]. It raised its [bold red]scythe[/bold red] and you got killed by it. Better luck next time!",
         "back": "room3",
         "end": True,
     },
@@ -62,23 +66,24 @@ rooms = {
 
 
 def show_room(room):
-    print(room["description"])
+    console.print(room["description"])
 
     if "forward" in room:
-        print("There is a path to move forward.")
+        console.print("There is a path to move [bold]forward[/bold].")
     if "back" in room:
-        print("There is a path to go back.")
+        console.print("There is a path to go [bold]back[/bold].")
     if "left" in room:
-        print("There is a path to your left.")
+        console.print("There is a path to your [bold]left[/bold].")
     if "right" in room:
-        print("There is a path to your right.")
+        console.print("There is a path to your [bold]right[/bold].")
     if "enter" in room:
-        print("You can enter the portal.")
+        console.print("You can [bold cyan]enter[/bold cyan] the portal.")
 
 
 def get_action(room):
     while True:
-        action = input("What do you want to do? ").lower().strip()
+        # make lowercase, remove whitespace and select first letter
+        action = input("What do you want to do? ").lower().strip()[0]
 
         if action == "f" and "forward" in room:
             return room["forward"]
@@ -91,7 +96,7 @@ def get_action(room):
         elif action == "e" and "enter" in room:
             return room["enter"]
         else:
-            print("Invalid action. Try again.")
+            console.print("[bold red]Invalid action. Try again.[/bold red]")
 
 
 current_room = rooms["start"]
@@ -101,4 +106,4 @@ while "end" not in current_room:
     next_room = get_action(current_room)
     current_room = rooms[next_room]
 
-print(current_room["description"])
+console.print(current_room["description"])
